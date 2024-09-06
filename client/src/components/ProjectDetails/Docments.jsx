153472b28241a6ac4394e034873c6d9f73cce239
tejudharanesh <tejudharanesh1234@gmail.com>
2024-09-06 16:31:00 +0530

    updated pages

--- client/src/components/ProjectDetails/Docments.jsx
+++ client/src/components/ProjectDetails/Docments.jsx
@@ -2,30 +2,68 @@ import React from "react";
 import pdf from "../../assets/images/pdf.png";
 import more from "../../assets/images/option.png";
 
-function Docments() {
+const sections = [
+  {
+    title: "Costing",
+    documents: [
+      {
+        id: 1,
+        name: "Tentative Quotation",
+        date: "18 July 2024",
+        version: "version1",
+      },
+      {
+        id: 2,
+        name: "Final Quotation",
+        date: "22 August 2024",
+        version: "version2",
+      },
+    ],
+  },
+  {
+    title: "Legal",
+    documents: [
+      {
+        id: 3,
+        name: "Contract Agreement",
+        date: "5 September 2024",
+        version: "version1",
+      },
+    ],
+  },
+];
+
+function Documents() {
   return (
     <div>
-      <p>Costing</p>
-      <div key="1" className="flex justify-between items-center mr-4">
-        <div className="flex items-center space-x-2">
-          <p className="text-black text-lg mr-2">1. </p>
-          <img src={pdf} alt="pdf icon" />
-          <div>
-            <p className="text-black text-lg">Tentative Quotation</p>
-            <p className="text-gray-400 text-sm">18 July 2024</p>
-          </div>
+      {sections.map((section, index) => (
+        <div key={index} className="px-2">
+          <p className="text-sm">{section.title}</p>
+          {section.documents.map((doc) => (
+            <div
+              key={doc.id}
+              className="flex justify-between items-center my-1"
+            >
+              <div className="flex items-center space-x-2">
+                <p className="text-black text-lg mr-2">{doc.id}.</p>
+                <img src={pdf} alt="pdf icon" />
+                <div>
+                  <p className="text-black text-lg">{doc.name}</p>
+                  <p className="text-gray-400 text-sm">{doc.date}</p>
+                </div>
+              </div>
+              <div className="flex items-center space-x-3">
+                <button className="bg-primaryO text-primary px-2 rounded-md border-[1px] border-primary mr-2">
+                  {doc.version}
+                </button>
+                <img src={more} alt="more" />
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="flex items-center space-x-3">
-          {1 && (
-            <button className="bg-primaryO text-primary px-2 rounded-md border-[1px] border-primary mr-2">
-              version1
-            </button>
-          )}
-          <img src={more} alt="more" />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
 
-export default Docments;
+export default Documents;
